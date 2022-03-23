605dabc71234d9e9eb2523f14f4ff54724f637fc
jishnuperiya <jishnuperiya@gmail.com>
2022-03-23 17:22:12 +0100

    added comments

--- Chapter02_Variables/ConsoleInput.c
+++ Chapter02_Variables/ConsoleInput.c
@@ -8,7 +8,7 @@ int main()
     double d;
 
     printf("Please enter a value for variable a: ");
-    scanf("%d", &a);
+    scanf("%d", &a); // you have to pass the address of the variable.
 
     printf("Please enter a value for variable b: ");
     scanf("%u", &b);
@@ -17,7 +17,7 @@ int main()
     scanf("%f", &c);
 
     printf("Please enter a value for variable d: ");
-    scanf("%lf", &d);
+    scanf("%lf", &d); // see here. the space holder for the double value is %lf
 
     printf("The value of variable a is: %d\n", a);
     printf("The value of variable b is: %u\n", b);
--- Chapter02_Variables/ConsoleOutput.c
+++ Chapter02_Variables/ConsoleOutput.c
@@ -3,14 +3,14 @@
 int main()
 {
     int a = 1;
-    unsigned int b = 1u;
-    float c = 1.0f;
+    unsigned int b = 1u; // remember to add u at the end of the variable value for unsigned integer values
+    float c = 1.0f;      // if you dont type f at the end, by default c will be double.
     double d = 1.0;
 
     printf("The value of variable a is: %d\n", a);
     printf("The value of variable b is: %u\n", b);
     printf("The value of variable c is: %f\n", c);
-    printf("The value of variable d is: %f\n", d);
+    printf("The value of variable d is: %f\n", d); // actually space holder for double is %lf
 
     return 0;
 }
