3458a3e34f0e7d4b8cf69474a1799110d59ce199
jishnuperiya <jishnuperiya@gmail.com>
2022-03-23 21:12:00 +0100

    added comments

--- Chapter02_Variables/VariableChar.c
+++ Chapter02_Variables/VariableChar.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-    char character_A = 'A';
+    char character_A = 'A'; // ascii code is 65, if you give 65, it will work
 
-    printf("Character: %d\n", character_A);
-    printf("Character: %c\n", character_A);
+    printf("Character: %d\n", character_A); //%d is for integer values-->it will print 65
+    printf("Character: %c\n", character_A); //%c is for character in text form --> it will print A
 
     char character_Z = character_A + 25;
 
-    printf("Character: %d\n", character_Z);
-    printf("Character: %c\n", character_Z);
+    printf("Character: %d\n", character_Z); // will be 90
+    printf("Character: %c\n", character_Z); // will be Z
 
     return 0;
 }
--- Chapter02_Variables/VariableRanges.c
+++ Chapter02_Variables/VariableRanges.c
@@ -10,14 +10,19 @@ int main()
     double d = 1.0;
     char e = 42;
 
-    printf("Variable a: %lu\n", sizeof(a));
+    printf(
+        "Variable a: %lu\n",
+        sizeof(
+            a)); // %lu stands for long unsigned int . because sizeof() always returns a positive value and can be very big
     printf("Variable b: %lu\n", sizeof(b));
     printf("Variable c: %lu\n", sizeof(c));
     printf("Variable d: %lu\n", sizeof(d));
     printf("Variable e: %lu\n", sizeof(e));
 
-    printf("Int range: %d, %d\n", INT_MIN, INT_MAX);
-    printf("Float range: %E, %E\n", FLT_MIN, FLT_MAX);
+    printf("Int range: %d, %d\n", INT_MIN, INT_MAX); // INT_MAX is the so called defined value
+    printf("Float range: %E, %E\n",
+           FLT_MIN,
+           FLT_MAX); // %E is the scientific notation. becuse float max value is very large
 
     return 0;
 }
